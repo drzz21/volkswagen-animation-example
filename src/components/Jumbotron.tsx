a0bfe784731bcac9544908605eb957e85d93a8b3
Francisco D <drz.y2k@gmail.com>
2023-07-13 16:00:00 -0600

    jumbotron

---
+++ src/components/Jumbotron.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Jumbotron = () => {
+	return (
+		<section className="container">
+			<div className="jumbotron"></div>
+			<div className='jumbotron-title'>Golf GTI W12
+			<button className='button'>Read more...</button>
+
+			</div>
+		</section>
+	);
+};
